e1bd51f9a25c6555c4f2e1b60b1c0e6fbd12a4f3
Unicorn <3038572935@qq.com>
2021-04-01 22:47:24 +0800

    fontconfig

---
+++ deps/fontconfig/Android.mk
@@ -0,0 +1,38 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE     := fontconfig
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
+# Additional font directories
+LOCAL_CFLAGS += -DFC_CACHEDIR=\"/sdcard/fontconfig/cache\"
+LOCAL_CFLAGS += -DFC_TEMPLATEDIR=\"/sdcard/fontconfig/conf.avail\"
+LOCAL_CFLAGS += -DFONTCONFIG_PATH=\"/sdcard/fontconfig/etc/fonts\"
+LOCAL_CFLAGS += -DFC_ADD_FONTS=\"/sdcard/fontconfig/fonts\"
+# System font directory
+LOCAL_CFLAGS += -DFC_DEFAULT_FONTS=\"/system/fonts\"
+
+include $(LOCAL_PATH)/src/Makefile.sources
+
+libfontconfig_la_SOURCES := $(filter-out fcarch.c, $(libfontconfig_la_SOURCES))
+
+LOCAL_SRC_FILES  := $(addprefix src/, $(filter %.c, $(libfontconfig_la_SOURCES)))
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/android \
+    $(LOCAL_PATH)/src \
+	$(LOCAL_PATH)/../freetype/include \
+	$(LOCAL_PATH)/../libexpat/lib \
+	$(LOCAL_PATH)/../libuuid/include
+
+LOCAL_STATIC_LIBRARIES := android_support freetype libexpat libuuid
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module,android/support)
+$(call import-module,deps/freetype)
+$(call import-module,deps/libexpat)
+$(call import-module,deps/libuuid)
