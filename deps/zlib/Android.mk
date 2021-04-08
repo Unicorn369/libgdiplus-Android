9c7f11364c4c07f4d9179c33c18ff68f9cb13613
Unicorn <3038572935@qq.com>
2021-04-08 13:17:46 +0800

    zlib

---
+++ deps/zlib/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := zlib
+
+LOCAL_SRC_FILES := \
+    adler32.c \
+    compress.c \
+    crc32.c \
+    deflate.c \
+    gzclose.c \
+    gzlib.c \
+    gzread.c \
+    gzwrite.c \
+    inflate.c \
+    infback.c \
+    inftrees.c \
+    inffast.c \
+    trees.c \
+    uncompr.c \
+    zutil.c
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+
+include $(BUILD_STATIC_LIBRARY)
