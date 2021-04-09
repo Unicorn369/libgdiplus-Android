3e766f4a405658d8e2a9d10e9dc9b751212f0d46
Unicorn <3038572935@qq.com>
2021-04-09 12:08:30 +0800

    rm

--- deps/pcre/Android.mk
+++ deps/pcre/Android.mk
@@ -6,14 +6,12 @@ include $(CLEAR_VARS)
 
 include $(LOCAL_PATH)/Sources.mk
 
-LOCAL_MODULE:= pcre
-LOCAL_SRC_FILES:= $(filter %.c, $(libpcre_la_SOURCES))
+LOCAL_MODULE := pcre
+LOCAL_SRC_FILES := $(filter %.c, $(libpcre_la_SOURCES))
 LOCAL_CFLAGS += $(libpcre_la_CPPFLAGS) -DNVALGRIND=1
-LOCAL_C_INCLUDES := $(GLIB_SOURCES_PATH) $(GLIB_SOURCES_PATH)/glib  \
-                    $(MAKEFILE_PATH)/../glib $(MAKEFILE_PATH)/../glib/include \
-                    $(MAKEFILE_PATH)/../libiconv/include
-
-LOCAL_STATIC_LIBRARIES := libiconv
+LOCAL_C_INCLUDES := $(GLIB_SOURCES_PATH) \
+                    $(GLIB_SOURCES_PATH)/glib  \
+                    $(MAKEFILE_PATH)/../glib \
+                    $(MAKEFILE_PATH)/../glib/include \
 
 include $(BUILD_STATIC_LIBRARY)
-$(call import-module,deps/libiconv)
