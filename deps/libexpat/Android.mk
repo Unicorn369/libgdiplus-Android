85b085392473a54184b473bddc3e6715c8ce8d67
Unicorn <3038572935@qq.com>
2021-04-11 14:39:43 +0800

    libexpat

--- deps/libexpat/Android.mk
+++ deps/libexpat/Android.mk
@@ -7,10 +7,10 @@ LOCAL_MODULE := libexpat
 LOCAL_CFLAGS := -DHAVE_EXPAT_CONFIG_H -DXML_ENABLE_VISIBILITY=1
 
 LOCAL_SRC_FILES := \
-    lib/xmlparse.c \
-    lib/xmlrole.c \
-    lib/xmltok.c
+    xmlparse.c \
+    xmlrole.c \
+    xmltok.c
 
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/lib
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
 
 include $(BUILD_STATIC_LIBRARY)
